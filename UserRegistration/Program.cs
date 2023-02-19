b7fdd4f400dd55495b6169ec1e7b8c7298fd6872
poojakrp777 <poojakrp777@gmail.com>
2023-02-19 20:44:34 +0530

    [POOJA] Validating the First name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -12,7 +12,14 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User registration validation");
-           
+            Console.WriteLine("Note: First name starts with Cap and has minimun 3 characters");
+
+            Console.WriteLine("Enter the first name");
+            string firstName = Console.ReadLine();
+
+            UserRegister userRegister = new UserRegister();
+            Console.WriteLine(userRegister.validateFirstName(firstName));
+            Console.ReadLine();
         }
     }
 }
---
+++ UserRegistration/UserRegister.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserRegister
+    {
+        public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        public bool validateFirstName(String firstName)
+        {
+            return Regex.IsMatch(firstName, NAME_PATTERN);
+        }
+    }
+}
