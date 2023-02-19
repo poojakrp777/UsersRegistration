900e5b770f6586630da12304a93732d6624e2a1b
poojakrp777 <poojakrp777@gmail.com>
2023-02-19 20:36:53 +0530

    Welcome to User Registration

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User registration validation");
+           
+        }
+    }
+}
