9fb5c7a47f335976997563e48b9bbac316a9eeba
poojakrp777 <poojakrp777@gmail.com>
2023-02-20 22:49:32 +0530

    [POOJA] Validating the password should have atleast 1 upper

--- UserRegistration/UserRegister.cs
+++ UserRegistration/UserRegister.cs
@@ -28,7 +28,7 @@ namespace UserRegistration
         {
             return Regex.IsMatch(mobileNoFormat, MOBILENOFORMAT_PATTERN);
         }
-        public static string PASSWORD_PATTERN = "^[0-9a-zA-Z]{8,}";
+        public static string PASSWORD_PATTERN = "^[A-Z]{1}[a-z]{7,}";
         public bool validatePassword(String password)
         {
             return Regex.IsMatch(password, PASSWORD_PATTERN);
