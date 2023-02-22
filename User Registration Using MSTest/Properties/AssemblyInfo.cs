89cd3b9f958b97ca87315002f2f187f71e190b40
poojakrp777 <poojakrp777@gmail.com>
2023-02-22 23:08:36 +0530

    [POOJA] Validating all using Unit test

---
+++ User Registration Using MSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("User Registration Using MSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("User Registration Using MSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be5cc764-dd1b-4711-a148-e5a2e22d43a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,43 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml;
-using System.Threading.Tasks;
+using UserRegistration;
 
 namespace UserRegistration
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User registration validation");
+            Console.WriteLine("Welcome to User Registration Programs using Regex");
 
-            UserRegister userRegister = new UserRegister();
-            Console.WriteLine("Note: First name starts with Cap and has minimun 3 characters");
-            Console.WriteLine("Enter the first name");
+            UserRegistration1 userRegistration = new UserRegistration1();
+
+            Console.WriteLine("Enter the first name = ");
             string firstName = Console.ReadLine();
-            Console.WriteLine(userRegister.validateFirstName(firstName));
+            Console.WriteLine(userRegistration.ValidateFirstName(firstName));
 
             Console.WriteLine("Enter the last name = ");
             string lastName = Console.ReadLine();
-            Console.WriteLine(userRegister.validateLastName(lastName));
+            Console.WriteLine(userRegistration.ValidateLastName(lastName));
 
             Console.WriteLine("Enter the email = ");
             string email = Console.ReadLine();
-            Console.WriteLine(userRegister.validateEmail(email));
+            Console.WriteLine(userRegistration.ValidateEmail(email));
 
-            Console.WriteLine("Enter the mobile no format = ");
-            string mobileNoFormat = Console.ReadLine();
-            Console.WriteLine(userRegister.validateMobileNoFormat(mobileNoFormat));
+            Console.WriteLine("Enter the phoneNumber = ");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine(userRegistration.ValidateMobileNoFormat(phoneNumber));
 
             Console.WriteLine("Enter the password = ");
             string password = Console.ReadLine();
-            Console.WriteLine(userRegister.validatePassword(password));
-
-            Console.WriteLine("Email sample validator");
-            EmailSample emailSamples = new EmailSample();
-            emailSamples.validateEmail();
+            Console.WriteLine(userRegistration.ValidatePassword(password));
             Console.ReadLine();
         }
     }
